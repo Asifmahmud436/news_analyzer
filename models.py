3e1ae7d321b9a857243fda70587877103fbeaabb
Asifmahmud436 <safaandsafa4@gmail.com>
2025-12-19 11:53:33 +0600

    added user model

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
-from sqlalchemy import JSON, DateTime, Text, Integer, Column
+from sqlalchemy import JSON, DateTime, String, Text, Integer, Column
 from datetime import datetime
 from database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
 
 class News(Base):
     __tablename__ = 'news'
